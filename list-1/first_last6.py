320fabe014c1da689ffc958d7de9d506287ab67c
ziyaydin <zaydin1@babson.edu>
2022-02-09 18:51:53 -0500

    Update first_last6.py

--- list-1/first_last6.py
+++ list-1/first_last6.py
@@ -2,7 +2,6 @@ def first_last6(nums):
     """Given an array of ints, return True if 6 appears as either the first or last element in the array. The array will be length 1 or more."""
     return nums[0] == 6 or nums[-1] == 6
 
-
 print(first_last6([1, 2, 6]))
 print(first_last6([6, 1, 2, 3]))
 print(first_last6([13, 6, 1, 2, 3]))
