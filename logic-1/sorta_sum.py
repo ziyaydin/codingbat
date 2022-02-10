11e1eb53721cbd5c09f0492ad7660a7836880eb2
ziyaydin <zaydin1@babson.edu>
2022-02-10 11:07:45 -0500

    Create sorta_sum.py

---
+++ logic-1/sorta_sum.py
@@ -0,0 +1,7 @@
+#Given 2 ints, a and b, return their sum. However, sums in the range 10..19 inclusive, are forbidden, so in that case just return 20.
+def sorta_sum(a, b):
+    if 10 <= a + b <= 19:
+        return 20
+    else:
+        return a + b
+
