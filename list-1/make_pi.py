5e92b0307645a6d12248a9fe521f8adedd5ea3a5
ziyaydin <zaydin1@babson.edu>
2022-02-09 19:39:25 -0500

    Create make_pi.py

---
+++ list-1/make_pi.py
@@ -0,0 +1,6 @@
+#return an int array length 3 containing the first 3 digits of pi, {3, 1, 4}.
+def make_pi():
+    """This function retunr sifrts 3 digits of pi"""
+    return [3, 1, 4]
+
+print(make_pi())
