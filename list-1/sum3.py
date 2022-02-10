bf1b8bc94ddf7a2aa8565e19f307db2056fc17e6
ziyaydin <zaydin1@babson.edu>
2022-02-09 19:39:31 -0500

    Create sum3.py

---
+++ list-1/sum3.py
@@ -0,0 +1,9 @@
+#Given an array of ints length 3, return the sum of all the elements
+
+def sum3(nums):
+    """get the same of all elements"""
+    return sum(nums)
+
+print(sum3([1, 2, 3]))
+print(sum3([5, 11, 2]))
+print(sum3([7, 0, 0]))
