ac7409855bf6b8e356781cf7805570d77e70fec8
ziyaydin <zaydin1@babson.edu>
2022-02-23 23:54:08 -0500

    Update near_ten.py

--- logic-1/near_ten.py
+++ logic-1/near_ten.py
@@ -1,6 +1,8 @@
 #Given a non-negative number "num", return True if num is within 2 of a multiple of 10. Note: (a % b) is the remainder of dividing a by b, so (7 % 5) is 2.
 def near_ten(num):
-  if num % 10 == 2:
+  if num % 2 in [0,1,2,8,9]:
     return True
+  else:
+    return False
 
 
