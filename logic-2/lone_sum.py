ba25bfc24aa851b82f6636bbb5580bff1cdd2dcd
ziyaydin <zaydin1@babson.edu>
2022-03-02 19:38:53 -0500

    Update lone_sum.py

--- logic-2/lone_sum.py
+++ logic-2/lone_sum.py
@@ -1,12 +1,21 @@
+#Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values, it does not count towards the sum.
 def lone_sum(a, b, c):
     """Given 3 int values, a b c, return their sum. However, if one of the values is the same as another of the values, it does not count towards the sum."""
-    l = [a, b, c]
-    result = 0
-    for x in l:
-        if l.count(x) == 1:
-            result += x
-    return result
-
+    # l = [a, b, c]
+    # result = 0
+    # for x in l:
+    #     if l.count(x) == 1:
+    #         result += x
+    # return result
+    if a == b == c:
+        return 0
+    if b == c:
+        return a
+    if a == c:
+        return b
+    if a == b:
+        return c  
+    return a + b + c
 
 print(lone_sum(1, 2, 3))
 print(lone_sum(3, 2, 3))
