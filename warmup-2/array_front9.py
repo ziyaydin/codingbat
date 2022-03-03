0a451ea633fd2d5d0039d5fdc605efaeddd2c918
ziyaydin <zaydin1@babson.edu>
2022-03-02 19:21:03 -0500

    Update array_front9.py

--- warmup-2/array_front9.py
+++ warmup-2/array_front9.py
@@ -1,11 +1,21 @@
 #Given an array of ints, return True if one of the first 4 elements in the array is a 9. The array length may be less than 4.
 
+# def array_front9(nums):
+#     for numbers in nums:
+#         if nums[0:4] == 9:
+#             return True
+#         else:
+#             return False
+
+
+
 def array_front9(nums):
-    for numbers in nums:
-        if nums[0:4] == 9:
+    first4 = nums[0:4]
+    for numbers in range(len(first4)):
+        if first4[numbers] == 9:
             return True
-        else:
-            return False
+    return False
+
 
 
 print(array_front9([1, 2, 9, 3, 4]))
