e1099dc57857290760994cc4e949ee11b347b340
ziyaydin <zaydin1@babson.edu>
2022-03-20 21:09:07 -0400

    Update string_bits.py

--- warmup-2/string_bits.py
+++ warmup-2/string_bits.py
@@ -1,10 +1,18 @@
 #Given a string, return a new string made of every other char starting with the first, so "Hello" yields "Hlo".
 
+# def string_bits(str):
+#     result = ''
+#     for letters in len(str):
+#         if letters % 2 == 0:
+#             result = result + letters
+#     print(result)
+
+# string_bits('Hello')
+
 def string_bits(str):
     result = ''
-    for letters in len(str):
-        if letters % 2 == 0:
-            result = result + letters
-    print(result)
+    for letters in range(0, len(str), 2):
+        result += str[letters]
+    return result
 
-string_bits('Hello')
+print(string_bits('Hello'))
