03ac74e1417a824a30ade19ae4b388463f683681
ziyaydin <zaydin1@babson.edu>
2022-03-28 10:13:11 -0400

    Update sum13.py

--- list-2/sum13.py
+++ list-2/sum13.py
@@ -1,8 +1,12 @@
 #Return the sum of the numbers in the array, returning 0 for an empty array. Except the number 13 is very unlucky, so it does not count and numbers that come immediately after a 13 also do not count.
-#WIPPPP
+#GOT HELP FROM https://gregorulm.com/coding-bat-python-list-2/
 def sum13(nums):
-    for i in len(nums):
-        if i == 13:
-            sum(nums[:i])
-        else:
-            return sum(nums)
+  if len(nums) == 0:
+    return 0
+ 
+  for i in range(0, len(nums)):
+    if nums[i] == 13:
+      nums[i] = 0
+      if i+1 < len(nums): 
+        nums[i+1] = 0
+  return sum(nums)
